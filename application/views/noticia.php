120e4000e82eb24ec3d0f75491289e58b99f5a60
Christian <Christian@SIS-CHRISTIAN>
2017-12-27 15:54:25 -0500

    noticia

--- application/views/noticia.php
+++ application/views/noticia.php
@@ -1,33 +1,33 @@
 <div class="container-fluid">
     <div class="container">
         <div class="white"></div>
-        <div class="row" style="margin-bottom: 50px"> 
+        <div class="row" style="margin-bottom: 50px">
             <div class="col-sm-offset-4 col-sm-4" style="margin-top: 35px">
-                <h1 style="font-family: MyriadPro-Bold; text-align: center">ÚLTIMAS NOTICIAS</h1>
+                <h1 style="font-family: MyriadPro-Bold; text-align: center;">ÚLTIMAS NOTICIAS</h1>
             </div> 
             <div class="col-sm-offset-5 col-sm-2">
                 <div class="red3"></div>
             </div>
         </div>
         <div class="row">
-            <div class="col-sm-offset-2 col-sm-8" style="background:transparent url('/PaginaWeb/assets/img/noticia2.jpg') no-repeat center center /cover; height: 285px;">
-                <h3 style="color: #fff">Lorem ipsum dolor sit amet consectetur adipiscing elit</h3>
-                <p style="color: #fff; text-align: justify;">
-                    Sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            <div class="col-sm-offset-2 col-sm-8" style="background: url('/PaginaWeb/assets/img/noticia2.jpg') no-repeat center center /cover; height: 285px;">
+                <h3 style="color: #fff; text-align: left;">Lorem ipsum dolor sit amet consectetur adipiscing elit</h3>
+                <p style="color: #fff; text-align: left;">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
             </div>
         </div>
         <div class="white"></div>
         <div class="row">
-            <div class="col-sm-offset-2 col-sm-4" style="background:transparent url('/PaginaWeb/assets/img/noticia1.jpg') no-repeat center center /cover; height: 200px;">
-                <h3 style="color: #fff">Lorem ipsum dolor sit amet consectetur adipiscing elit</h3>
-                <p style="color: #fff; text-align: justify;">
+            <div class="col-sm-offset-2 col-sm-4 noticia-last" style="background: url('/PaginaWeb/assets/img/noticia1.jpg') no-repeat center center /cover; height: 200px; margin-right: 18px;">
+                <h3 style="color: #fff; text-align: right;">Lorem ipsum dolor sit amet consectetur adipiscing elit</h3>
+                <p style="color: #fff; text-align: right;">
                     Sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
                 </p>
             </div>
-            <div class="col-sm-4" style="background:transparent url('/PaginaWeb/assets/img/noticia3.jpg') no-repeat center center /cover; height: 200px;">
-                <h3 style="color: #fff">Lorem ipsum dolor sit amet consectetur adipiscing elit</h3>
-                <p style="color: #fff; text-align: justify;">
+            <div class="col-sm-4 noticia-last" style="background: url('/PaginaWeb/assets/img/noticia3.jpg') no-repeat center center /cover; height: 200px; margin-left: 18px;">
+                <h3 style="color: #fff; text-align: left;">Lorem ipsum dolor sit amet consectetur adipiscing elit</h3>
+                <p style="color: #fff; text-align: left;">
                     Sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
                 </p>
             </div>
@@ -50,17 +50,41 @@
         </div>
         <div class="white"></div>
         <div class="row">
-            <div class="col-sm-3">
-                <div style="background-color: #000; height: 100px"></div>
+            <div class="col-sm-offset-2 col-sm-3 noticia-otros" style="background: url('/PaginaWeb/assets/img/noticia3.jpg') no-repeat center center /cover; height: 500px; margin-bottom: 25px;">
+                <h3 style="color: #fff; text-align: left;">Lorem ipsum dolor sit amet consectetur adipiscing elit</h3>
+                <p style="color: #fff; text-align: left;">
+                    Sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                </p>
             </div>
-            <div class="col-sm-3">
-                <div style="background-color: #000; height: 100px"></div>
+            <div class="col-sm-3 noticia-otros" style="background: #e1e1e1; height: 300px; margin: 0 25px;">
+                <h3 style="color: #fff; text-align: left;">Lorem ipsum dolor sit amet consectetur adipiscing elit</h3>
+                <p style="color: #fff; text-align: left; ">
+                    Sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                </p>
             </div>
-            <div class="col-sm-3">
-                <div style="background-color: #000; height: 100px"></div>
+            <div class="col-sm-3 noticia-otros" style="background: url('/PaginaWeb/assets/img/noticia1.jpg') no-repeat center center /cover; height: 500px; margin-bottom: 25px;">
+                <h3 style="color: #fff; text-align: left;">Lorem ipsum dolor sit amet consectetur adipiscing elit</h3>
+                <p style="color: #fff; text-align: left;">
+                    Sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                </p>
             </div>
-            <div class="col-sm-3">
-                <div style="background-color: #000; height: 100px"></div>
+            <div class="col-sm-offset-2 col-sm-3 noticia-otros" style="background: #e1e1e1; height: 300px;">
+                <h3 style="color: #fff; text-align: left;">Lorem ipsum dolor sit amet consectetur adipiscing elit</h3>
+                <p style="color: #fff; text-align: left;">
+                    Sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                </p>
+            </div>
+            <div class="col-sm-3 noticia-otros" style="background: url('/PaginaWeb/assets/img/noticia2.jpg') no-repeat center center /cover; height: 500px; margin: -200px 25px 0 25px;">
+                <h3 style="color: #fff; text-align: left;">Lorem ipsum dolor sit amet consectetur adipiscing elit</h3>
+                <p style="color: #fff; text-align: left;">
+                    Sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                </p>
+            </div>
+            <div class="col-sm-3 noticia-otros" style="background: #e1e1e1; height: 300px; ">
+                <h3 style="color: #fff; text-align: left;">Lorem ipsum dolor sit amet consectetur adipiscing elit</h3>
+                <p style="color: #fff; text-align: left;">
+                    Sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                </p>
             </div>
         </div>
         <div class="white"></div>
